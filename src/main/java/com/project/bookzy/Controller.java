414cd6fbbee88e6d45be657a10591822f90e2477
bkchan96 <39748671+bkchan96@users.noreply.github.com>
2021-02-07 17:31:40 -0800

    Added mapping to Controller.java public function

--- src/main/java/com/project/bookzy/Controller.java
+++ src/main/java/com/project/bookzy/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class Controller {
 
-        @RequestMapping("/")
+        @RequestMapping("/bookzy")
         public String index() {
                 return "Welcome to the Bookzy web app!";
         }
